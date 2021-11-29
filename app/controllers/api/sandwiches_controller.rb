48cc53ac65160b8273d0a9d6bb75b1240a1581eb
Ruby Reed <rubysreed@gmail.com>
2021-11-29 15:06:35 -0700

    finished rails back end

--- app/controllers/api/sandwiches_controller.rb
+++ app/controllers/api/sandwiches_controller.rb
@@ -1,7 +1,47 @@
 class Api::SandwichesController < ApplicationController
+  before_action:set_sandwich, only: [:show, :destroy, :update]
 
+  #get /api/sandwiches
   def index
     render json:Sandwich.all
   end
 
+    # get /api/sandwiches/:id
+    def show
+      render json: @sandwich
+    end
+  
+    # post /api/sandwiches
+    def create
+      @sandwich = Sandwich.new(sandwich_params)
+      if (@sandwich.save)
+        render json: @sandwich
+      else
+        render json: { errors: @sandwich.errors }, status: :unprocessable_entity
+      end
+    end
+  
+    # put/patch /api/sandwiches/:id
+    def update
+      if (@sandwich.update(sandwich_params))
+        render json: @sandwich
+      else
+        render json: { errors: @sandwich.errors }, status: :unprocessable_entity
+      end
+    end
+  
+    # delete /api/sandwiches/:id
+    def destroy
+      render json: @sandwich.destroy
+    end
+  
+    private
+  
+    def sandwich_params
+      params.require(:sandwich).permit(:name, :price, :description)
+    end
+  
+    def set_sandwich
+      @sandwich = Sandwich.find(params[:id])
+    end
 end
